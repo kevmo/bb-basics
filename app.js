30126717b7f53cf710ba6ebca6c83ab51f5f3b46
kevmo <k.james.moore@gmail.com>
2015-01-21 07:50:33 -0600

    WIP

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ app.get('/', function (req, res){
   res.send('HARO');
 });
 
+app.post('/', function(req, res){
+  res.send('lol post lol');
+});
+
+app.put('/user', function(req, res){
+  res.send('lol put lol');
+});
+
+app.delete('/', function(req,res){
+  res.send('lol delete lol');
+});
+
 var server = app.listen(3000, function(){
 
   var host = server.address().address;
